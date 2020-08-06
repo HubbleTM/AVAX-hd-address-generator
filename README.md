249f229b456d9d11ab345f9d1ffcb67a31905928
Emre Kanatli <kanatliemre@gmail.com>
2020-08-06 16:09:48 -0400

    initial and done

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+## Prerequisites
+
+NodeJs v12.14.1
+
+## Setup
+
+1- Clone the repo `git clone https://github.com/ava-labs/hd-addresses.git`
+2-Install dependencies `npm install`
+
+## Running the project
+Simply run `node index.js` at the root of the project.
---
+++ index.js
@@ -0,0 +1,51 @@
+var prompt = require('prompt');
+const HDKey = require('hdkey');
+const bip39 = require('bip39');
+const Buffer = require('buffer/').Buffer;
+const Avalanche = require('avalanche');
+const AVAX_ACCOUNT_PATH = `m/44'/9000'/0'`;
+
+try{
+    prompt.start();
+    console.log("How many addresses do you want to generate?")
+    prompt.get(['num_addr'], (err, result) => {
+        let addrNum = parseInt(result.num_addr);
+        console.log("\nEnter your mnemonic phrase:")
+        prompt.get(['mnemonic'], (err, result) => {
+            let mnemonic = result.mnemonic;
+            let addresses = generateAddresses(mnemonic, addrNum);
+            console.log('\n');
+            for(var i=0; i<addresses.length; i++){
+                console.log(`${i}:\t${addresses[i]}`);
+            }
+        });
+    })
+}catch(e){
+    console.error(e);
+}
+
+
+function generateAddresses(mnemonicIn, num){
+    let keychain = new Avalanche.AVMKeyChain('X');
+
+    let mnemonic = mnemonicIn.trim();
+    const seed = bip39.mnemonicToSeedSync(mnemonic)
+    const hdkey = HDKey.fromMasterSeed(seed)
+
+    let keys = [];
+    for(var i=0; i<num; i++){
+        let derivationPath = `${AVAX_ACCOUNT_PATH}/0/${i}`;
+        let key = hdkey.derive(derivationPath)
+        keys.push(key);
+    }
+
+    let addrs = keys.map((key) => {
+        let pkHex = key.privateKey.toString('hex');
+        let pkBuf = new Buffer(pkHex, 'hex');
+        let addr = keychain.importKey(pkBuf);
+        let keypair = keychain.getKey(addr)
+        return keypair.getAddressString();
+    });
+
+    return addrs;
+}
